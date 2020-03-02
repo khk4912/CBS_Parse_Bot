5d8936dfbf93b1bd3e96d7c17d427f4724a56d88
khk4912 <30457148+khk4912@users.noreply.github.com>
2020-03-03 00:23:36 +0900

    .

--- cogs/cbs.py
+++ cogs/cbs.py
@@ -19,11 +19,13 @@ class CBS(commands.Cog):
         async with aiohttp.ClientSession() as session:
             async with session.get(self.CBSList) as r:
                 data = await r.json()
+                
         embed = discord.Embed(
             title="📢 재난문자",
             description="최근 발송된 3개의 재난문자를 보여줘요.",
             color=0xE71212
         )
+        
         for i in data[:3]:
             embed.add_field(name=i["SJ"], value=i["CONT"], inline=False)
         await ctx.send(embed=embed)
